fc57c727db84296bac2de402f4f23aec767800fb
Juan Manuel Palacios <juan980424@gmail.com>
2021-07-04 12:32:56 -0500

    requests get post put delete

---
+++ models/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    //Middlewares
+    this.middlewares();
+    //Rtas de mi aplicación
+    this.router();
+  }
+
+  middlewares() {
+    //Directorio publico
+    this.app.use(express.static('public'));
+  }
+
+  router() {
+    this.app.get('/api', (req, res) => {
+      res.json({
+        msg: 'Get Api',
+      });
+    });
+
+    this.app.put('/api', (req, res) => {
+      res.json({
+        msg: 'Put Api',
+      });
+    });
+
+    this.app.post('/api', (req, res) => {
+      res.json({
+        msg: 'Post Api',
+      });
+    });
+
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        msg: 'Delete Api',
+      });
+    });
+  }
+
+  lister() {
+    this.app.listen(this.port, () => {
+      console.log(`Example app listening at http://localhost:${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
