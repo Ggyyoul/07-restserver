d532401ffd159ab0dcf5ab2e2ea07a129df50b10
Juan Manuel Palacios <juan980424@gmail.com>
2021-07-05 11:22:40 -0500

    encriptando la contraseña

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,16 @@
-const { response, request } = require('express');
-const User = require('../models/user')
+const { response } = require('express');
+const bcryptjs = require('bcryptjs');
 
+const User = require('../models/user');
 
 const usersGet = (req, res = response) => {
   // http://localhost:8080/api/users?q=hola&apiKey=1664684
-  const {q, name = 'No name', apiKey} = req.query;
+  const { q, name = 'No name', apiKey } = req.query;
   res.json({
     msg: 'get API - controller',
     q,
     name,
-    apiKey
+    apiKey,
   });
 };
 
@@ -22,13 +23,19 @@ const usersPut = (req, res = response) => {
 };
 
 const usersPost = async (req, res = response) => {
+  const { name, email, password, role } = req.body;
+  const user = new User({ name, email, password, role });
 
-  const body = req.body;
-  const user = new User(body)
+  // confirm if email exist
+
+  //Encryp password
+  const salt = bcryptjs.genSaltSync();
+  user.password = bcryptjs.hashSync(password, salt);
+  //save in DB
   await user.save();
 
   res.status(201).json({
-    user
+    user,
   });
 };
 
