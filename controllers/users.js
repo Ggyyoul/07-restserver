d31a228699f81f224ab21a6f7810c7ee5c1a8518
Juan Manuel Palacios <juan980424@gmail.com>
2021-07-04 19:07:06 -0500

    Parametros por segmento y por query

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,32 @@
 const { response, request } = require('express');
 
 const usersGet = (req, res = response) => {
+  // http://localhost:8080/api/users?q=hola&apiKey=1664684
+  const {q, name = 'No name', apiKey} = req.query;
+
   res.json({
     msg: 'get API - controller',
+    q,
+    name,
+    apiKey
+
   });
 };
 
 const usersPut = (req, res = response) => {
+  const id = req.params.id;
   res.json({
     msg: 'Put Api - controller',
+    id,
   });
 };
 
 const usersPost = (req, res = response) => {
-
-  const {name, age} = req.body;
+  const { name, age } = req.body;
   res.status(201).json({
     msg: 'Post Api - controller',
     name,
-    age
+    age,
   });
 };
 
@@ -31,5 +39,5 @@ module.exports = {
   usersGet,
   usersPut,
   usersPost,
-  usersDelete
+  usersDelete,
 };
